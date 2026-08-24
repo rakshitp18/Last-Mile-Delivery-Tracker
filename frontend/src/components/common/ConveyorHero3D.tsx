c477ed336a1f6b8a2a30d292fcf057d14b2310fc
rakshitp18 <rakshitpandey1806@gmail.com>
2026-08-24 23:24:57 +0530

    feat: shift 3D conveyor animation further left into the hero viewport

--- frontend/src/components/common/ConveyorHero3D.tsx
+++ frontend/src/components/common/ConveyorHero3D.tsx
@@ -363,7 +363,7 @@ export const ConveyorHero3D: React.FC<ConveyorHero3DProps> = ({
 
     // 5. World Root Container
     const worldRoot = new THREE.Group();
-    worldRoot.position.set(-0.6, 0, 0.4);
+    worldRoot.position.set(-1.6, 0, 0.8);
     scene.add(worldRoot);
 
     // 5.1 Conveyor Assembly
